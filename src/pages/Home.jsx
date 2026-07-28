fd1f772c0be9a87527347ded734e3f3254112beb
Dyluan <dylan.devoir@gmail.com>
2026-07-28 11:26:46 +0200

    almost finished home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,10 @@ import bigBimmer from '../assets/bmw_m1_big.jpg';
 import leftArrow from '../assets/left-arrow.png';
 import engineBay from '../assets/engine-bay.png';
 import steering from '../assets/steering-wheel.png';
+import garage from '../assets/garage.png';
+import m1 from '../assets/m1-side.png';
+import e30 from '../assets/e30.png';
+import v12 from '../assets/v12lmr.png';
 
 function Home() {
   return(
@@ -15,7 +19,7 @@ function Home() {
           <div className="heroMiddle font-[Anybody] uppercase text-[#E5E2E1] font-extrabold text-[88px] italic">L'héritage en <span className="text-[#E20613]">mouvement</span></div>
           <div className="heroBottom flex flex-row items-center gap-8">
             <div className="heroButton">
-              <button className="bg-[#E20613] text-[18px] font-[JetBrains_Mono] text-[#FFF3F2] px-10 py-5 flex flex-row items-center gap-4">
+              <button className="bg-[#E20613] text-[18px] font-[JetBrains_Mono] text-[#FFF3F2] px-10 py-5 flex flex-row items-center gap-4 hover:bg-[#0066B3] cursor-pointer">
                 <div className="heroButtonText">Découvrir les légendes</div>
                 <div className="heroButtonImg"><img src={leftArrow} alt="arrow" /></div>
               </button>
@@ -32,6 +36,7 @@ function Home() {
         <div className="lineMiddle bg-[#282572] h-full w-full"></div>
         <div className="lineRight bg-[#E20613] h-full w-full"></div>
       </div>
+      {/* partie description */}
       <div className="descriptionSection w-full max-h-[742px]">
         <div className="content flex flex-row px-16 py-32">
           <div className="leftDescription max-w-[45%]">
@@ -58,6 +63,72 @@ function Home() {
             </div>
           </div>
         </div>
+      </div>
+      {/* partie chronologie */}
+      <div className="chronologySection w-full bg-[#131313]">
+        <div className="title flex flex-col items-center pt-32">
+          <div className="top uppercase text-[#E20613] font-[JetBrains_Mono] font-bold text-[12px] mb-4 tracking-[1.2px]">chronologie</div>
+          <div className="bottom capitalize text-[#E5E2E1] font-[Anybody] font-bold text-[40px] pb-24">les jalons de la victoire</div>
+        </div>
+        <div className="chronoContent relative flex flex-col">
+          <div className="line absolute top-0 bottom-0 left-1/2 -translate-x-1/2 w-0.5 bg-[#343434]"></div>
+          <div className="content relative flex items-center">
+            <div className="left flex flex-col items-end max-w-[50%] w-[50%]">
+              <div className="top text-[#FFB4AA] font-[JetBrains_Mono] text-2xl mb-2">1972</div>
+              <div className="middle text-[#E5E2E1] font-[Anybody] font-bold text-[32px] mb-3">Création de BMW Motorsport</div>
+              <div className="bottom font-[Geist] text-[#E9BCB6] text-[16px] text-right">Une petite équipe de 35 passionnés s'installe à Munich avec un seul but : dominer la compétition mondiale.</div>
+            </div>
+            <div className="divider w-0.5 mx-2 flex flex-col items-center justify-center">
+              <div className="ball h-4 w-4 bg-[#E20613] rounded-[50%]"></div>
+            </div>
+            <div className="right max-w-[50%] w-[50%]">
+              <img src={garage} alt="garage" />
+            </div>
+          </div>
+          <div className="content relative flex items-center pt-24">
+            <div className="left max-w-[50%] w-[50%] flex justify-end">
+              <img src={m1} alt="m1 Procar" />
+            </div>
+            <div className="divider w-0.5 mx-2 flex flex-col items-center justify-center">
+              <div className="ball h-4 w-4 bg-[#0568B5] rounded-[50%]"></div>
+            </div>
+            <div className="right flex flex-col items-start max-w-[50%] w-[50%]">
+              <div className="top text-[#A2C9FF] font-[JetBrains_Mono] text-2xl mb-2">1978</div>
+              <div className="middle text-[#E5E2E1] font-[Anybody] font-bold text-[32px] mb-3">L'Avènement de la M1</div>
+              <div className="bottom font-[Geist] text-[#E9BCB6] text-[16px] text-left">La première voiture à porter fièrement le badge M. Un chef- d'œuvre à moteur central conçu pour la piste et la route.</div>
+            </div>
+          </div>
+          <div className="content relative flex items-center pt-24">
+            <div className="left flex flex-col items-end max-w-[50%] w-[50%]">
+              <div className="top text-[#FFB4AA] font-[JetBrains_Mono] text-2xl mb-2">1987</div>
+              <div className="middle text-[#E5E2E1] font-[Anybody] font-bold text-[32px] mb-3">M3 E30 : Reine du DTM</div>
+              <div className="bottom font-[Geist] text-[#E9BCB6] text-[16px] text-right">Une petite équipe de 35 passionnés s'installe à Munich avec un seul but : dominer la compétition mondiale.</div>
+            </div>
+            <div className="divider w-0.5 mx-2 flex flex-col items-center justify-center">
+              <div className="ball h-4 w-4 bg-[#2D2A77] rounded-[50%]"></div>
+            </div>
+            <div className="right max-w-[50%] w-[50%]">
+              <img src={e30} alt="e30 DTM" />
+            </div>
+          </div>
+          <div className="content relative flex items-center pt-24">
+            <div className="left max-w-[50%] w-[50%] flex justify-end">
+              <img src={v12} alt="v12 LMR" />
+            </div>
+            <div className="divider w-0.5 mx-2 flex flex-col items-center justify-center">
+              <div className="ball h-4 w-4 bg-[#E20613] rounded-[50%]"></div>
+            </div>
+            <div className="right flex flex-col items-start max-w-[50%] w-[50%]">
+              <div className="top text-[#A2C9FF] font-[JetBrains_Mono] text-2xl mb-2">1978</div>
+              <div className="middle text-[#E5E2E1] font-[Anybody] font-bold text-[32px] mb-3">L'Avènement de la M1</div>
+              <div className="bottom font-[Geist] text-[#E9BCB6] text-[16px] text-left">La première voiture à porter fièrement le badge M. Un chef- d'œuvre à moteur central conçu pour la piste et la route.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* partie "rejoignez la légende" */}
+      <div className="joinSection">
+        
       </div>
     </div>
   )
